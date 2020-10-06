5f9e5eafb2ad8b9ef26b09b9eca224f2af6a14d8
Kingsley Adio <adiksonline@gmail.com>
2020-10-06 23:23:11 +0200

    Add RLP adn RDP packages

---
+++ app/src/main/java/com/deliveryhero/workshop/dc2020/ui/rdp/RdpViewModel.kt
@@ -0,0 +1,5 @@
+package com.deliveryhero.workshop.dc2020.ui.rdp
+
+import androidx.lifecycle.ViewModel
+
+class RdpViewModel : ViewModel()
---
+++ app/src/main/java/com/deliveryhero/workshop/dc2020/ui/rlp/RlpViewModel.kt
@@ -0,0 +1,5 @@
+package com.deliveryhero.workshop.dc2020.ui.rlp
+
+import androidx.lifecycle.ViewModel
+
+class RlpViewModel : ViewModel()
